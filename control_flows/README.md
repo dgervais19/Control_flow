67a136aeed19a0c10d2d91160299027a6f2e4c1f
Donovan Gervais <applemac@Donovans-MacBook-Pro-6.local>
2020-11-03 15:57:26 +0000

    Age restriction exercise

---
+++ control_flows/README.md
@@ -0,0 +1,6 @@
+# Python Control Flows
+
+*THIS LESSON WILL COVER CONTROL FLOW*
+
+## If statements, elif, and else statements
+### for loops and while loops
--- control_flows/control_flow.py
+++ control_flows/control_flow.py
@@ -2,7 +2,33 @@
 # if statements
 # Syntax: if then condition
 
-age = 15
+# age = 15
+#
+# if age > 15:
+#     print("Thank you, you are old enough to watch the movie")
+#
+# elif age <= 15:
+#     print("Sorry you are too young.... try again when you grow up")
+#
+# else:
+#     print("OOPs sorry something went wrong, try again later")
 
-if age >= 15:
-    print("Thank you, you are old enough to watch the movie")
+
+# create a program using control flow with if, elif and else
+# using operators == >=
+# check age restrictions before selling the ticket
+# 18, 15, U, 12, PG
+
+age = 19
+
+if age < 3:
+    print("Way too young, you should watch a film rated U")
+elif age <= 11:
+    print("You are in need of Parental Guidance (PG)")
+elif age <= 14:
+    print("You're not quite there yet. Watch TV 12")
+elif age < 18:
+    print("Almost old enough to watch 18, but still too young")
+else:
+    print("You can watch any film you like")
+
