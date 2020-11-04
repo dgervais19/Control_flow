b27d83ad060d4246be64102d5ab5182b2f40507e
Donovan Gervais <applemac@Donovans-MacBook-Pro-6.local>
2020-11-04 10:35:47 +0000

    polished up notes on for loops

--- control_flows/for_loops.py
+++ control_flows/for_loops.py
@@ -2,14 +2,16 @@
 # for loop is used to iterate through the data
 # Syntax for variable_name in name_of_data_collection_variable
 
-# shopping_list = ["eggs", "milk", "super malt"]
-# print(shopping_list)
-#
-# for items in shopping_list:
-#     if items == "milk":
-#         print(items)
+# creating a list called shopping list
+shopping_list = ["eggs", "milk", "super malt"]
+print(shopping_list)
 
+# Creating a for loop that searches the list incrementally. Once it reaches "Milk" it prints that item
+for items in shopping_list:
+    if items == "milk":
+        print(items)
 
+# creating a dictionary called user_details
 user_details = {
     "key": "value",
     "name": "Dono",
@@ -18,9 +20,11 @@ user_details = {
     "hobbies": ["basketball", "Piano", "Gym", "Socialising", "data types"]
 }
 
+# creating a for loop that seaeches each key in the dictionary and prints it
 for keys in user_details:
     print(keys)
 
-# for values in user_details:
-#     print(values)
+# # creating a for loop that searches each value in the dictionary and prints it
+for values in user_details:
+    print(values)
 
